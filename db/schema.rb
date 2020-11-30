4f42b6578739d86777b2b4f1de8f46b3091f0969
EC2 Default User <ec2-user@ip-172-31-35-226.ec2.internal>
2020-11-30 17:05:43 +0000

    genre model追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_165437) do
+ActiveRecord::Schema.define(version: 2020_11_30_170157) do
 
   create_table "cart_products", force: :cascade do |t|
     t.integer "customer_id"
     t.integer "product_id"
     t.integer "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_active", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
