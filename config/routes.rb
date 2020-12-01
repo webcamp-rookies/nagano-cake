96609152d61db795ef197577c0b4ff2e5cae22ac
EC2 Default User <ec2-user@ip-172-31-35-226.ec2.internal>
2020-12-01 07:01:43 +0000

    route記述変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins
+  devise_for :customers, controllers:{
+  sessions:      'customers/sessions',
+  passwords:     'customers/passwords',
+  registrations: 'customers/registrations'
+    
+  }
+  devise_for :admins, controllers: {
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
 end
