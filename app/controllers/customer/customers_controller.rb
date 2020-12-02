f4032cc9eb0ae36fd3144def3cd709ba13e1c7ee
EC2 Default User <ec2-user@ip-172-31-35-226.ec2.internal>
2020-12-02 05:02:37 +0000

    顧客情報編集画面

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -1,16 +1,11 @@
 class Customer::CustomersController < ApplicationController
+    
     def edit
-        @customer = current_customer
+    @customer = current_customer
     end
     
-    def update
-            @customer = current_customer
-    	if @customer.update(customer_params)
-           flash[:success] = "登録情報を変更しました"
-           redirect_to customers_path
-        else
-           render :edit and return
-        end
-	end
+    def show
+    @customer = current_customer
+    end
 
 end
