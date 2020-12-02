6b47ab731a87929c14e499d39ee2b95eca9a57b2
EC2 Default User <ec2-user@ip-172-31-35-226.ec2.internal>
2020-12-02 13:40:57 +0000

    linkbtn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
-      before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  def after_sign_in_path_for(resource)
+    products_about_path #要変更
+  end
   protected
 
   def configure_permitted_parameters
