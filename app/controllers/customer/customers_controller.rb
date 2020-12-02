f6dd86efd3e2ea41f3b4d25c46b5c4b75060d8cd
EC2 Default User <ec2-user@ip-172-31-66-74.ec2.internal>
2020-12-02 08:24:44 +0000

    [complete]about_view

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -1,9 +1,9 @@
 class Customer::CustomersController < ApplicationController
-    
+
     def edit
     @customer = current_customer
     end
-    
+
     def show
     @customer = current_customer
     end
