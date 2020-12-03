404db162f58323ab0879ee75b0fad024c741c716
EC2 Default User <ec2-user@ip-172-31-35-226.ec2.internal>
2020-12-03 05:36:21 +0000

    退会機能実装開始　customerコントローラ変更

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -15,15 +15,28 @@ class Customer::CustomersController < ApplicationController
     end
     
     def show
-        @customer = current_customer
+        @customer = Customer.find(params[:id])
+    end
+    
+    def check
+        @user = User.find(params[:id])
+        #ユーザーの情報を見つける
+    end
+
+    
+    def withdraw
+        @customer = Customer.find(current_customer.id)
+        @customer.update(is_deleted: false)
+        reset_session
+        redirect_to root_path
     end
 
 
     private
     def customer_params
-    params.require(:customer).permit(:last_name, :first_name, 
-                                    :last_name_kana, :first_name_kana,
-                                    :email, :postcode, :city, :phone_number)
+        params.require(:customer).permit(:last_name, :first_name, 
+                                        :last_name_kana, :first_name_kana,
+                                        :email, :postcode, :city, :phone_number)
     end
 
 end
