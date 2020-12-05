be3e4918d6e1c9bf1ab8bc871c48bc4f8e46e4f5
EC2 Default User <ec2-user@ip-172-31-66-74.ec2.internal>
2020-12-05 13:14:34 +0000

    [add]cart_products_controller

---
+++ app/controllers/customer/cart_products_controller.rb
@@ -0,0 +1,18 @@
+class Customer::CartProductsController < ApplicationController
+  
+  def index
+  end
+  
+  def update
+  end
+  
+  def create
+  end
+  
+  def destroy
+  end
+  
+  def destroy_all
+  end
+  
+end
