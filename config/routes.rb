18f7b62b653f65ac14eafa580869bb801b251f31
EC2 Default User <ec2-user@ip-172-31-35-226.ec2.internal>
2020-12-07 02:21:12 +0000

    カート機能完成

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   scope module: :customer do
 
     root 'products#top'
+    delete 'cart_products/destroy_all' => 'cart_products#destroy_all'
     get "about" => "products#about"
     resources :products
 
@@ -26,6 +27,7 @@ Rails.application.routes.draw do
     end
     resources :ship_cities,only: [:index,:create,:edit,:update,:destroy]
     resources :cart_products
+
 
   end
 end
