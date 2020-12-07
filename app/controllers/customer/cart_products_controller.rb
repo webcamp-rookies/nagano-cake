36c81b7a1fe9efa4b0e72b1f6f80faddbd555ce5
EC2 Default User <ec2-user@ip-172-31-66-74.ec2.internal>
2020-12-07 02:25:35 +0000

    [update]cart_product

--- app/controllers/customer/cart_products_controller.rb
+++ app/controllers/customer/cart_products_controller.rb
@@ -14,8 +14,14 @@ class Customer::CartProductsController < ApplicationController
   def create
     @cart_product = CartProduct.new(cart_product_params)
     @cart_product.customer_id = current_customer.id
-    @cart_product.save
-    redirect_to cart_products_path
+    if current_customer.cart_products.find_by(product_id: params[:cart_product][:product_id]).present?
+      cart_product = current_customer.cart_products.find_by(product_id: params[:cart_product][:product_id])
+      cart_product.amount += params[:cart_product][:amount].to_i
+      cart_product.save
+      redirect_to cart_products_path
+    else @cart_product.save
+      redirect_to cart_products_path
+    end
   end
 
   def update
