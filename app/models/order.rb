a11a76580b811ea841ac945e5c0be9e217045fb8
EC2 Default User <ec2-user@ip-172-31-66-74.ec2.internal>
2020-12-09 07:50:17 +0000

    [add]layout

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,7 +7,7 @@ class Order < ApplicationRecord
 
   enum status: {入金待ち:0, 入金確認:1, 製作中:2, 発送準備中:3, 発送済み:4}
 
-  validates :postcode, presence: true, format: {with: /\A\d{7}\z/}
+  validates :postcode, presence: true
   validates :city, presence: true
   validates :name, presence: true
 
